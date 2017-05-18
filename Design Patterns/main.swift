189f7af488ac0d6cdab605dca03cc695b360a7f8
Ethan Lillie <lillieethan@gmail.com>
2017-05-18 11:36:09 -0700

    Initial Commit

---
+++ Design Patterns/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Design Patterns
+//
+//  Created by Ethan Lillie on 5/18/17.
+//  Copyright © 2017 ME. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
